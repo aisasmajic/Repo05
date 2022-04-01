18e5e28a3b8098aed5936b24226373c76993688d
unknown <aisa.smajic27@gmail.com>
2022-04-01 08:57:52 +0200

    Adding name

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // only change code below this line
+    var myFirstName = "Ajisa";
+    var myLastName = "Smajic";
+    var myHomeTown = "Tuzla";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
